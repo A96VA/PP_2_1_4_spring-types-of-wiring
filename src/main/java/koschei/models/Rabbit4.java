99060c9be747c6dcc55c9444e3e33fd3a35bdc02
Viktor Aleynikov <A96VA@yandex.ru>
2022-11-14 17:51:19 +0500

    4

--- src/main/java/koschei/models/Rabbit4.java
+++ src/main/java/koschei/models/Rabbit4.java
@@ -1,12 +1,18 @@
 package koschei.models;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Rabbit4 {
+    private final Duck5 rabbit;
+    @Autowired
+    public Rabbit4(Duck5 rabbit) {
+        this.rabbit = rabbit;
+    }
 
     @Override
     public String toString() {
-        return ", в зайце утка " + "";
+        return ", в зайце утка " + rabbit.toString();
     }
 }
